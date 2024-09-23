71eca8b5e4bc97df6fbde9db51631976c26560c3
maxRechem <mvr@tafiliconsulting.be>
2024-09-23 12:13:56 +0200

    browser

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter  as Router, Route, Routes } from 'react-router-dom';
 import Home from '../Home/Home';
 import Mbc from '../Mbc/Mbc';
 import Finance from '../Finance/Finance';
@@ -10,26 +10,26 @@ import PowerBI from '../powerbi/PowerBI';
 import Yetoo from '../Yetoo/Yetoo';
 import About from '../AboutAndTeam/AboutAndTeam';
 import DataAnalyst from '../DataAnalyst/DataAnalyst';
-import Header from '../Header/Header';
 
 function App() {
   return (
-    <Router basename="/tafili-consulting">
-      <Header />
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/mbc" element={<Mbc />} />
-          <Route path="/financial" element={<Finance />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/powerbi" element={<PowerBI />} />
-          <Route path="/yetoo" element={<Yetoo />} />
-          <Route path="/dataanalyst" element={<DataAnalyst />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Router>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/home" element={<Home/>} />
+                <Route path="/website" element={<Home/>} />
+                <Route path="/mbc" element={<Mbc/>} />
+                <Route path="/financial" element={<Finance/>} />
+                <Route path="/contact" element={<Contact/>} />
+                <Route path="/team" element={<Team/>} />
+                <Route path="/powerbi" element={<PowerBI/>} />
+                <Route path="/yetoo" element={<Yetoo/>} />
+                <Route path="/about" element={<About/>} />
+                <Route path="/dataanalyst" element={<DataAnalyst/>} />
+            </Routes>
+        </Router>
+    </div>
   );
 }
 
